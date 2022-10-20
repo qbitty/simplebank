92bb1ebaa8fb9614541a3968106e12a635c88604
qbaby <lzhp1988@gmail.com>
2022-10-20 17:59:28 +0800

    transfer api test

--- api/transfer.go
+++ api/transfer.go
@@ -12,7 +12,7 @@ import (
 type TransferRequest struct {
 	FromAccountID int64  `json:"from_account_id" binding:"required,min=1"`
 	ToAccountID   int64  `json:"to_account_id" binding:"required,min=1"`
-	Amount        int64  `json:"amount" binding:"required,ge=0"`
+	Amount        int64  `json:"amount" binding:"required,gt=0"`
 	Currency      string `json:"currency" binding:"required,currency"`
 }
 
