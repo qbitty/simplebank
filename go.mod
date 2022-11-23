5d77e96ad3bb03e08a23f60c736ea2b81f37f56e
qbitty <liumc0710@gmail.com>
2022-11-23 23:56:18 +0800

    zerolog

--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/lib/pq v1.10.7
 	github.com/o1egl/paseto v1.0.0
 	github.com/rakyll/statik v0.1.7
+	github.com/rs/zerolog v1.15.0
 	github.com/spf13/viper v1.13.0
 	github.com/stretchr/testify v1.8.0
 	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4
--- util/config.go
+++ util/config.go
@@ -7,6 +7,7 @@ import (
 )
 
 type Config struct {
+	Environment          string        `mapstructure:"ENVIRONMENT"`
 	DBDriver             string        `mapstructure:"DB_DRIVER"`
 	DBSource             string        `mapstructure:"DB_SOUECE"`
 	MigrationURL         string        `mapstructure:"MIGRATION_URL"`
